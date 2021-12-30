da973c5c944df6c4f7c0eeaa7f860c9a2059cedf
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-30 15:02:59 +0330

    Add files via upload

---
+++ Pseudo Random.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+
+using namespace std;
+int main()
+
+{
+	for (int i=0; i<8; i++)
+	
+	cout<<rand()<<endl;
+	cout<<"RAND_MAX : "<<RAND_MAX<<endl;
+	
+	getch();
+	
+}
